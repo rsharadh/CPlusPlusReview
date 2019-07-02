9fa542ce7521ec278cc0737e13d510c0991099ef
Sharadh Ramaswamy <rsharadh@sharadh-ramaswamys-macbook-pro.local>
2019-07-02 12:38:56 -0700

    Adding code for integral images and connected components.

---
+++ connected_component.cc
@@ -0,0 +1,333 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+#include <deque>
+
+#define IS_DEBUG 0
+
+namespace {
+
+template<class T>
+T** Init(const int height, const int width) {
+	T** data;
+	if (height * width > 0) {
+		data = new T*[height];
+		data[0] = new T[height * width];
+		for (unsigned int row_index = 1; 
+			row_index < height; 
+			row_index++) {
+			data[row_index] = data[row_index - 1] +  width; 
+		}
+
+		for (unsigned int row = 0; row < height; row++) {
+			for (unsigned int col = 0; col < width; col++) {
+				data[row][col] = 0;
+			}
+		}
+	} else {
+		data = nullptr;
+	}
+	return data;
+}
+
+
+template <class T>	
+void Print(T** data, const int height, const int width) {
+	for (unsigned int row = 0; row < height; row++) {
+		for (unsigned int col = 0; col < width; col++) {
+			std::cout<<static_cast<int>(data[row][col])<<" "; 
+		}
+		std::cout<<std::endl;
+	}	
+}	
+
+
+void Print(const std::vector<std::vector<int>>& label_equivalences,
+	const std::string& display_string) {
+#if IS_DEBUG	== 1
+	std::cout<<display_string <<std::endl;
+	for (int label_index = 0; 
+		label_index < label_equivalences.size(); 
+		label_index++) {
+		for (auto& label: label_equivalences[label_index]) {
+			std::cout<<label_index + 1<<" <=> "<<label<<std::endl;
+		}
+	}
+#endif	
+}
+
+void Print(
+	const std::vector<int>& label_equivalences_for,
+	const int label_of_interest,
+	const std::string& display_string) {
+#if IS_DEBUG == 1
+	std::cout<<display_string <<std::endl;
+	for (auto label : label_equivalences_for) {
+		std::cout<<label_of_interest<<" <=> "<<label<<std::endl;
+	}
+#endif
+}
+
+void Print(
+	const std::vector<int>& label_equivalences_for,
+	const int label_of_interest,
+	const int equivalent_label,
+	const std::string& display_string) {
+#if IS_DEBUG == 1
+	std::cout<<display_string <<std::endl;
+	for (auto label : label_equivalences_for) {
+		std::cout<<label_of_interest<<" <=> "
+			<<label<<" == "
+			<<equivalent_label<<std::endl;
+	}
+#endif	
+}
+
+void Print(const std::vector<int>& equivalent_labels,
+	const std::string& display_string) {
+#if IS_DEBUG == 1
+	std::cout<<display_string <<std::endl;
+	for (int label_index = 0; 
+		label_index < equivalent_labels.size(); 
+		label_index++) {
+		std::cout<<label_index + 1<<" <=> "
+			<<equivalent_labels[label_index]<<std::endl;
+	}
+#endif	
+}
+
+template<class T>
+class Image {
+public:
+	Image() : height_(0), width_(0), data_(nullptr) {}
+
+	Image(const int height, const int width) : height_(height), width_(width) {
+		data_ = Init<T>(height_, width_);
+		labels_ = Init<int>(height_, width_);
+	}
+
+	~Image() {
+		if (data_ != nullptr) {
+			delete[] data_;
+		}
+	}
+
+	Image(const Image&) = delete;
+	Image(Image&&) = delete;
+	Image& operator=(const Image&) = delete;
+	Image& operator=(Image&&) = delete;
+
+	const T* operator[](const int row) const {
+		if (row < height_ && row >= 0) {
+			return data_[row];
+		} else {
+			throw std::string("Accesing invalid element");
+		}
+	}
+
+	T* operator[](const int row) {
+		if (row < height_ && row >= 0) {
+			return data_[row];
+		} else {
+			throw std::string("Accesing invalid element");
+		}
+	}
+
+	void rand_init(const T min = 0, const T max = 255) {
+		unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+		//std::cout<<"seed "<<seed<<std::endl;
+		//unsigned seed = 1892949966;
+  		std::default_random_engine generator (seed);
+  		for (unsigned int row = 0; row < height_; row++) {
+			for (unsigned int col = 0; col < width_; col++) {
+				std::uniform_int_distribution<T> distribution(min, max);
+				data_[row][col] = distribution(generator);
+			}
+		}
+	}
+
+	void add_label(int* num_labels, 
+		std::vector<std::vector<int>>* label_equivalences) {
+		(*num_labels) += 1;
+		label_equivalences->push_back(std::vector<int>());
+	}
+
+	bool is_label_in_equivalences(
+		const std::vector<int>& label_equivalences_for, 
+		const int label_of_interest) {
+		bool is_label_in_equivalences_list = false;
+		for (const auto& label : label_equivalences_for) {
+			if (label == label_of_interest) {
+				is_label_in_equivalences_list = true;
+				break;
+			}
+		}
+		return is_label_in_equivalences_list;
+	}
+
+	void update_label_equivalences( 
+		std::vector<std::vector<int>>* label_equivalences, 
+		const int west_label, const int north_label) {
+		if (west_label == north_label) {
+			return;
+		}
+		if (!is_label_in_equivalences(
+				(*label_equivalences)[north_label-1], west_label)) {
+			(*label_equivalences)[north_label-1].push_back(
+				west_label);
+		}
+
+		if (!is_label_in_equivalences(
+				(*label_equivalences)[west_label-1], north_label)) {
+			(*label_equivalences)[west_label-1].push_back(
+				north_label);
+		}
+	}
+
+	void find_connected_components() {
+		int num_labels = 0;
+		std::vector<std::vector<int>> label_equivalences;
+ 		for (unsigned int row = 0; row < height_; row++) {
+			for (unsigned int col = 0; col < width_; col++) {
+				if (row == 0 && col == 0) {
+					if (data_[row][col] != 0) {
+						add_label(&num_labels, &label_equivalences);
+						labels_[row][col] = num_labels;
+					}
+					continue;
+				}
+				T current_pixel = data_[row][col];
+				if (current_pixel == 0) {
+					continue;
+				}
+				if (row == 0) {
+					T west_pixel = data_[row][col - 1];
+					if (current_pixel == west_pixel) {
+						labels_[row][col] = labels_[row][col - 1];
+					} else {
+						add_label(&num_labels, &label_equivalences);
+						labels_[row][col] = num_labels;
+					}
+				}
+				if (col == 0) {
+					T north_pixel = data_[row - 1][col];
+					if (current_pixel == north_pixel) {
+						labels_[row][col] = labels_[row - 1][col];
+					} else {
+						add_label(&num_labels, &label_equivalences);
+						labels_[row][col] = num_labels;
+					}
+				}
+				if (row > 0 && col > 0) {
+					T north_pixel = data_[row - 1][col];
+					T west_pixel = data_[row][col - 1];
+					int north_label = labels_[row - 1][col];
+					int west_label = labels_[row][col - 1];
+					if (north_pixel == 0 && west_pixel == 0) {
+						add_label(&num_labels, &label_equivalences);
+						labels_[row][col] = num_labels;
+						continue;
+					}
+					if (north_pixel == 0) {
+						labels_[row][col] = west_label;
+						continue;
+					}
+					if (west_pixel == 0) {
+						labels_[row][col] = north_label;
+						continue;
+					}
+					update_label_equivalences(&label_equivalences, west_label, north_label);
+					labels_[row][col] = west_label;
+				}
+			}
+		}
+
+		Print(label_equivalences, "Equivalent labels after first pass");
+		std::vector<int> equivalent_labels(num_labels, 0);
+		int really_unique_labels = 0;
+		for (int label_index = 0; 
+			label_index < label_equivalences.size(); 
+			label_index++) {
+			if (equivalent_labels[label_index] != 0) {
+				continue;
+			}
+			really_unique_labels++;	
+			equivalent_labels[label_index] = really_unique_labels;
+ 			if (label_equivalences[label_index].empty()) {
+ 				continue;
+ 			} 
+
+			Print(
+				label_equivalences[label_index], 
+				label_index + 1,
+				really_unique_labels, 
+				"Equivalent labels outside queue");
+
+			std::deque<int> label_queue;
+			for (auto& label: label_equivalences[label_index]) {
+				label_queue.push_back(label);
+			}
+
+			while (!label_queue.empty()) {
+				int current_label = label_queue.front();
+				label_queue.pop_front();
+				if (equivalent_labels[current_label - 1] != 0) {
+					continue;
+				}
+				equivalent_labels[current_label - 1] = 
+					really_unique_labels;
+				auto& label_equivalences_for_current_label = 
+					label_equivalences[current_label - 1];	
+
+				Print(
+					label_equivalences_for_current_label, 
+					current_label,
+					"Equivalent labels inside queue");							
+				for (
+					auto& label : 
+					label_equivalences_for_current_label) {							
+					if (equivalent_labels[label - 1] == 0) {
+						label_queue.push_back(label);
+					}
+				}
+			}
+		}
+		Print(equivalent_labels, "Equivalent Labels identified");
+
+ 		for (unsigned int row = 0; row < height_; row++) {
+			for (unsigned int col = 0; col < width_; col++) {
+				int original_label = labels_[row][col];
+				if (original_label == 0) {
+					continue;
+				}
+				labels_[row][col] = equivalent_labels[original_label - 1];
+			}
+		}
+	}
+
+	T** ptr() const {return data_;}
+	int** label_ptr() const {return labels_;}
+	int width() const {return width_;}
+	int height() const {return height_;}
+
+private:
+  int height_;
+  int width_;
+  T** data_;
+  int** labels_;
+};
+
+}  // namespace
+
+int main(int argc, char** argv) {
+	int num_rows = 4;
+	int num_cols = 4;
+	Image<int> image(num_rows, num_cols);
+	image.rand_init(0, 1);
+	std::cout<<"printing image after rand_init"<<std::endl;
+	Print<int>(image.ptr(), image.height(), image.width());
+	image.find_connected_components();
+	std::cout<<"printing image connected components"<<std::endl;
+	Print<int>(image.label_ptr(), image.height(), image.width());
+	return 0;
+}
---
+++ integral_image.cc
@@ -0,0 +1,192 @@
+#include <iostream>
+#include <chrono>
+#include <random>
+
+namespace {
+
+template<class T>
+T** Init(const int height, const int width) {
+	T** data;
+	if (height * width > 0) {
+		data = new T*[height];
+		data[0] = new T[height * width];
+		for (unsigned int row_index = 1; 
+			row_index < height; 
+			row_index++) {
+			data[row_index] = data[row_index - 1] +  width; 
+		}
+
+		for (unsigned int row = 0; row < height; row++) {
+			for (unsigned int col = 0; col < width; col++) {
+				data[row][col] = 0;
+			}
+		}
+	} else {
+		data = nullptr;
+	}
+	return data;
+}		
+
+
+template<class T>
+class Image {
+public:
+	Image() : height_(0), width_(0), data_(nullptr) {}
+
+	Image(const int height, const int width) : height_(height), width_(width) {
+		data_ = Init<T>(height_, width_);
+		sum_ =  Init<long>(height_, width_);
+	}
+
+	~Image() {
+		if (data_ != nullptr) {
+			delete[] data_;
+		}
+		if (sum_ != nullptr) {
+			delete[] sum_;
+		}
+	}
+
+	Image(const Image&) = delete;
+	Image(Image&&) = delete;
+	Image& operator=(const Image&) = delete;
+	Image& operator=(Image&&) = delete;
+
+	const T* operator[](const int row) const {
+		if (row < height_ && row >= 0) {
+			return data_[row];
+		} else {
+			throw std::string("Accesing invalid element");
+		}
+	}
+
+	T* operator[](const int row) {
+		if (row < height_ && row >= 0) {
+			return data_[row];
+		} else {
+			throw std::string("Accesing invalid element");
+		}
+	}
+
+	double sum() const {
+		double sum;
+		for (unsigned int row = 0; row < height_; row++) {
+			for (unsigned int col = 0; col < width_; col++) {
+				sum += static_cast<double>(data_[row][col]);
+			}
+		}
+		return sum;
+	}
+
+	void rand_init(const T min = 0, const T max = 255) {
+		unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  		std::default_random_engine generator (seed);
+  		for (unsigned int row = 0; row < height_; row++) {
+			for (unsigned int col = 0; col < width_; col++) {
+				std::uniform_int_distribution<T> distribution(min, max);
+				data_[row][col] = distribution(generator);
+			}
+		}
+	}
+
+	void integrate() {
+		sum_[0][0] = data_[0][0];
+		unsigned int col = 0;
+		for (unsigned int row = 1; row < height_; row++) {
+			sum_[row][col] +=  sum_[row-1][col] + data_[row][col];
+		}
+		unsigned int row = 0;
+		for (unsigned int col = 1; col < width_; col++) {
+			sum_[row][col] += sum_[row][col-1] + data_[row][col];
+		}
+  		for (unsigned int row = 1; row < height_; row++) {
+			for (unsigned int col = 1; col < width_; col++) {
+				sum_[row][col] = 
+					+ sum_[row - 1][col] 
+					+ sum_[row][col - 1]
+					- sum_[row - 1][col - 1] 
+					+ data_[row][col]; 
+			}
+		}		
+	}
+
+	double sum(int row_min, int col_min, int row_max, int col_max) {
+		if (row_max < 0 || col_max < 0 || row_max >= height_ || col_max >= width_) {
+			throw std::string("Out of bounds!!");
+		}
+		if (row_min > row_max || col_min > col_max) {
+			throw std::string("Out of bounds!!");
+		}
+		double sum = 0;
+		sum += static_cast<double>(sum_[row_max][col_max]);
+		if (row_min >= 0 && col_min >= 0 && row_min < height_ && col_min < width_) {
+				sum += static_cast<double>(sum_[row_min][col_min]);
+		}
+		if (row_min >=0 && row_min < height_) {
+				sum += static_cast<double>(-sum_[row_min][col_max]);
+		}
+		if (col_min >=0 && col_min < width_) { 
+			sum += static_cast<double>(-sum_[row_max][col_min]);
+		}
+		return sum; 
+	}
+
+	void print() {
+		for (unsigned int row = 0; row < height_; row++) {
+			for (unsigned int col = 0; col < width_; col++) {
+				std::cout<<static_cast<int>(data_[row][col])<<" "; 
+			}
+			std::cout<<std::endl;
+		}	
+	}
+
+	void print_sum() {
+		for (unsigned int row = 0; row < height_; row++) {
+			for (unsigned int col = 0; col < width_; col++) {
+				std::cout<<sum_[row][col]<<" "; 
+			}
+			std::cout<<std::endl;
+		}	
+	}
+private:
+  int height_;
+  int width_;
+  T** data_;
+  long** sum_;
+};
+
+}  // namespace
+
+int main(int argc, char** argv) {
+
+	// Image<unsigned char> uchar_image(100, 100);
+	// std::cout<<"uchar_image sum "<<uchar_image.sum()<<std::endl;
+	// uchar_image.rand_init();
+	// std::cout<<"uchar_image sum "<<uchar_image.sum()<<std::endl;
+	// uchar_image.integrate();
+	// std::cout<<"uchar_image sum "<<uchar_image.sum(0, 0, 99, 99)<<std::endl;
+	
+	int num_rows = 5;
+	int num_cols = 5;
+	Image<unsigned char> int_image(num_rows, num_cols);
+	std::cout<<"int_image sum "<<int_image.sum()<<std::endl;
+	int_image.rand_init(0, 1);
+	std::cout<<"printing image after rand_init"<<std::endl;
+	int_image.print();
+	std::cout<<"int_image[0, "<< num_cols-1 <<"] "
+	<<int_image[0][num_cols-1]<<std::endl;
+	std::cout<<"int_image["<<num_rows-1<< ", "
+		<<num_cols-1<<"] "<<int_image[num_rows-1][num_cols-1]
+		<<std::endl;
+	std::cout<<"int_image sum "<<int_image.sum()<<std::endl;
+	std::cout<<"integrating image "<<std::endl;
+
+	int_image.integrate();
+	std::cout<<"printing image sum after integrate"<<std::endl;
+	int_image.print_sum();
+	std::cout<<"int_image sum (-1, -1, "<<num_rows-1<<", "
+		<<num_cols-1<<") "
+		<<int_image.sum(-1, -1, num_rows-1, num_cols-1)
+		<<std::endl;
+	return 0;
+}
